2376e1e79dd7336615176e8df6efc41fee97c80b
ahmed hassanin <ahmed.hassasnin@nweave.com>
2024-04-06 13:15:03 +0200

    Make new page for not-found and update route page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { HomeLayoutComponent } from './home-layout/home-layout.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
-  { path: 'signin', component: SignInComponent },
-  { path: 'signup', component: SignUpComponent },
   { path: '', component: HomeLayoutComponent },
   { path: 'dashborad', redirectTo: '', pathMatch: 'full' },
+  { path: 'signin', component: SignInComponent },
+  { path: 'signup', component: SignUpComponent },
+  { path: '**', component: NotFoundComponent },
 ];
